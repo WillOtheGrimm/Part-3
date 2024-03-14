4b17a65460d5fe6059959a9e2b0dcf1a3834388c
WILLO_LAPTOP\ayman <ayman010905@gmail.com>
2024-03-14 15:25:22 -0400

    Finished task 4b

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI showSelected;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -12,8 +15,22 @@ public class CharacterControl : MonoBehaviour
         {
             SelectedVillager.Selected(false);
         }
+
+
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
     }
-    
+ 
+
+    private void Update()
+    {
+        if (SelectedVillager != null)
+        {
+        showSelected.text = SelectedVillager.CanOpen().ToString();
+        }
+        else
+        {
+            showSelected.text = "No selection";
+        }
+    }
 }
